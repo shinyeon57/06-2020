9d68a619f24afa5a5a7fba785c3195583e2db52a
shinyeon57 <noeys57@sookmyung.ac.kr>
2020-10-08 16:45:29 +0900

    practice 04

--- main.c
+++ main.c
@@ -2,15 +2,53 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-
-int square(int a)
+int factorial(int n)
 {
-	return(a*a);
+	int res=1;
+	int i	;
+	
+	for(i=1;i <= n; i++)
+		{
+		 res = res * i;
+		}
+	return res;
 }
-int	main()
+
+int get_integer()
 {
-	int a = 2;
-	a = square(a);
-	printf("a=%i\n",a);
+	int x;
+	printf("enter a number :");
+	scanf("%i", &x);
+	
+	return x;
+}
+
+int combination(int n, int r)
+{
+	int n1, n2, n3;
+	
+	n1 = factorial(n);
+	n2 = factorial(n-r);
+	n3 = factorial(r);
+	
+	return	(n1/n2/n3);
+	
+}
+
+int main(int argc, char*argv[])
+{
+	int n;
+	int r;
+	int result;
+	
+	n = get_integer();
+	r = get_integer();
+
+	
+	result = combination(n,r);
+	printf("calcombination result is %d\n", result);
+	
+	return 0;
+	
+	
 }
